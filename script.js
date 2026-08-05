6f6be4e62262a229bdaa59ba2657cd02f141b544
Christopher <chris.kuut@pm.me>
2026-08-05 21:26:03 +0300

    ilusat p2eva teile k6igile ja palju seksi (added table to show books and buttons to add books)

--- script.js
+++ script.js
@@ -1,4 +1,12 @@
 const myLibrary = [];
+let titleGlobal;
+let authorGlobal;
+
+const titleButton = document.getElementById("title");
+const authorButton = document.getElementById("author");
+const createBookButton = document.getElementById("create");
+const showBookButton = document.getElementById("show");
+const bookTableBody = document.querySelector("tbody");
 
 function Book(title, author) {
   this.title = title;
@@ -6,11 +14,43 @@ function Book(title, author) {
   this.id = crypto.randomUUID();
 }
 
-function getBook() {
-  const book = new Book(prompt("What is the book title?"), prompt("What is the book title?"));
+function createBook(title, author) {
+  const book = new Book(title, author);
   myLibrary.push(book);
+  addBookToTable(book);
 }
 
-getBook();
+function addBookToTable(book) {
+  const row = bookTableBody.insertRow(-1);
+  const headerRow = document.getElementById("id-for-column-count");
+  const columns = headerRow.querySelectorAll("th");
+  for (i = 0; i < columns.length; i++) {
+    const cell = row.insertCell(i);
+    switch (i) {
+      case 0:
+        cell.innerHTML = myLibrary.length;
+        break;
+      case 1:
+        cell.innerHTML = myLibrary[myLibrary.length - 1].title;
+        break;
+      case 2:
+        cell.innerHTML = myLibrary[myLibrary.length - 1].author;
+        break;
+      case 3:
+        cell.innerHTML = myLibrary[myLibrary.length - 1].id;
+        break;
+    }
+  }
+}
 
-console.log(myLibrary[0]);
+titleButton.addEventListener("click", (e) => {
+  titleGlobal = prompt("What is the book title?");
+});
+
+authorButton.addEventListener("click", (e) => {
+  authorGlobal = prompt("What is the author's name?");
+});
+
+createBookButton.addEventListener("click", (e) => {
+  createBook(titleGlobal, authorGlobal);
+});
