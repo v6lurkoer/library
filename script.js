b6af470742d48dee597724a87ef46f79e3398d42
Christopher <chris.kuut@pm.me>
2026-08-12 13:24:11 +0300

    commiting before disaster

--- script.js
+++ script.js
@@ -1,29 +1,25 @@
 const myLibrary = [];
-let titleGlobal;
-let authorGlobal;
 
-const titleButton = document.getElementById("title");
-const authorButton = document.getElementById("author");
-const createBookButton = document.getElementById("create");
-const showBookButton = document.getElementById("show");
-const bookTableBody = document.querySelector("tbody");
-
-function Book(title, author) {
+function Book(title, author, pages, read) {
   this.title = title;
   this.author = author;
+  this.pages = pages;
+  this.read = read;
   this.id = crypto.randomUUID();
 }
 
-function createBook(title, author) {
-  const book = new Book(title, author);
+function createBook(title, author, pages, read) {
+  const book = new Book(title, author, pages, read);
   myLibrary.push(book);
   addBookToTable(book);
 }
 
 function addBookToTable(book) {
-  const row = bookTableBody.insertRow(-1);
-  const headerRow = document.getElementById("id-for-column-count");
+  const tableBody = document.querySelector("tbody");
+  const row = tableBody.insertRow(-1);
+  const headerRow = document.getElementById("idForColumnCount");
   const columns = headerRow.querySelectorAll("th");
+
   for (i = 0; i < columns.length; i++) {
     const cell = row.insertCell(i);
     switch (i) {
@@ -37,20 +33,41 @@ function addBookToTable(book) {
         cell.innerHTML = myLibrary[myLibrary.length - 1].author;
         break;
       case 3:
+        cell.innerHTML = myLibrary[myLibrary.length - 1].pages;
+        break;
+      case 4:
+        cell.innerHTML = myLibrary[myLibrary.length - 1].read;
+        break;
+      case 5:
         cell.innerHTML = myLibrary[myLibrary.length - 1].id;
         break;
     }
   }
 }
 
-titleButton.addEventListener("click", (e) => {
-  titleGlobal = prompt("What is the book title?");
-});
+function submit() {
+  const title = document.getElementById("title").value;
+  const author = document.getElementById("author").value;
+  const pages = document.getElementById("pages").value;
+  const read = document.querySelector("input[name=read]:checked").value;
 
-authorButton.addEventListener("click", (e) => {
-  authorGlobal = prompt("What is the author's name?");
-});
+  createBook(title, author, pages, read);
 
-createBookButton.addEventListener("click", (e) => {
-  createBook(titleGlobal, authorGlobal);
+  document.getElementById("bookForm").reset();
+}
+
+const btn = document.getElementById("addBookButton");
+let expanded = 0;
+btn.addEventListener("click", (e) => {
+  const body = document.querySelector("body");
+  const form = document.getElementById("formSection");
+  if (expanded === 0) {
+    body.style.gridTemplateColumns = "1fr 3fr";
+    form.style.display = "block";
+    expanded = 1;
+  } else if (expanded === 1) {
+    body.style.gridTemplateColumns = "50px 3fr";
+    form.style.display = "none";
+    expanded = 0;
+  }
 });
